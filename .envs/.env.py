281337d1bd7c677c8aef9becb14bc26d39d972c3
Smirnova Tanya <75753895+SmirnovaT@users.noreply.github.com>
2023-01-30 17:50:06 +0300

    social_network

---
+++ .envs/.env.py
@@ -0,0 +1,8 @@
+POSTGRES_PASSWORD=postgres
+DB_HOST = localhost
+DB_PORT = 5432
+DB_USER = postgres
+DB_NAME = postgres
+DB_PASS = postgres
+
+DB_DSN=${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+from database import Base, engine
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
